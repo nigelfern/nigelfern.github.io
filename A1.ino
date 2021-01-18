17af4c58f371c648a93f2f2ace917a29788ffb18
nigelfern <55825785+nigelfern@users.noreply.github.com>
2021-01-17 21:55:57 -0800

    Add files via upload

--- A1.ino
+++ A1.ino
@@ -1,50 +1,37 @@
 /*
-  Blink
+ * Nigel Fernandes
+ * A1 Blink
+ * HCDE 439
+ * 1/14/21
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+  Turns three different LED's on for 0.5 seconds, then off for 0.5 seconds, repeatedly.
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT); // initialize digital pin 9 as an output.
+  pinMode(10, OUTPUT); // initialize digital pin 10 as an output.
+  pinMode(11, OUTPUT); // initialize digital pin 11 as an output.
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+
+  //Blue LED is mapped to pin 9
+  digitalWrite(9, HIGH);   // turn the blue LED on (HIGH is the voltage level)
+  delay(500);              // wait for 0.5 seconds
   digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);
-  //second
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+  delay(500);              // wait for 0.5 seconds
+
+  //Yellow LED is mapped to pin 10
+  digitalWrite(10, HIGH);   // turn the yellow LED on (HIGH is the voltage level)
+  delay(500);               // wait for 0.5 seconds
   digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);
+  delay(500);               // wait for 0.5 seconds
   
-  
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+  //Red LED is mapped to pin 11
+  digitalWrite(11, HIGH);   // turn the red LED on (HIGH is the voltage level)
+  delay(500);               // wait for 0.5 seconds
   digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);
+  delay(500);               // wait for 0.5 seconds
 }
