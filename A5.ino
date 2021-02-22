5b3a2f62d5515e05345c5f706aec43ca5faf2ba9
nigelfern <55825785+nigelfern@users.noreply.github.com>
2021-02-22 00:30:15 -0800

    Add files via upload

---
+++ A5.ino
@@ -0,0 +1,81 @@
+/*
+ * Nigel Fernandes
+ * A5 Higher Voltage and Transistors
+ * HCDE 439
+ * 2/22/21
+
+Changes LED brightness based on received signals from Remote. Turns on motor when remote's Power button is pressed
+LED gets brighter when higher numbers are pressed and is less bright when lower numbers are pressed.
+*/
+
+/*
+ * Specify which protocol(s) should be used for decoding.
+ * If no protocol is defined, all protocols are active.
+ */
+#define DECODE_NEC 1
+
+#include <IRremote.h>
+
+int IR_RECEIVE_PIN = 10; //receives signals at 10
+const int motorPin =  3; // motors signals at pin 3
+const int ledPin = 6; // LED Pin is set to 6
+
+void setup() {
+  pinMode(6, OUTPUT); //initializes pin 6 for output
+    Serial.begin(115200); // begins serial monitor
+    // Prints the program that is running
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    /*
+     * Start the receiver, enable feedback LED and take LED feedback pin from the internal boards definition
+     */
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);//, USE_DEFAULT_FEEDBACK_LED_PIN);
+
+    Serial.print(F("Ready to receive IR signals at pin ")); //Prints that the Arduino is ready to receive from remote
+    Serial.println(IR_RECEIVE_PIN); //prints the pin number where signals are received at
+}
+
+void loop() {
+    /*
+     * Check if received data is available and if yes, try to decode it.
+     * 
+     */
+    if (IrReceiver.decode()) {
+        switch (IrReceiver.decodedIRData.command) {//Decoded result is in the IrReceiver.decodedIRData structure.
+          case 0x45: Serial.println("Power button"); //Prints which button is pressed
+          digitalWrite(motorPin, HIGH); //turns on motor
+             break;
+          case 0x47: Serial.println("Func/Stop"); //Prints which button is pressed
+          digitalWrite(ledPin, LOW); //turns off red LED
+             break;
+          case 0x0C: Serial.println("1"); //Prints which button is pressed
+          analogWrite(ledPin, 10); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x18: Serial.println("2"); //Prints which button is pressed
+          analogWrite(ledPin, 40); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x5E: Serial.println("3"); //Prints which button is pressed
+          analogWrite(ledPin, 70); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x08: Serial.println("4"); //Prints which button is pressed
+          analogWrite(ledPin, 100); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x1C: Serial.println("5"); //Prints which button is pressed
+          analogWrite(ledPin, 130); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x5A: Serial.println("6"); //Prints which button is pressed
+          analogWrite(ledPin, 160); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x42: Serial.println("7"); //Prints which button is pressed
+          analogWrite(ledPin, 190); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x52: Serial.println("8"); //Prints which button is pressed
+          analogWrite(ledPin, 220); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+          case 0x4A: Serial.println("9"); //Prints which button is pressed
+          analogWrite(ledPin, 250); // Sends appropriate amount of brightness based on which button is pressed
+             break;
+        }
+        IrReceiver.resume(); // Enable receiving of the next value      
+    }
+}
